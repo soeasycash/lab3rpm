d81334518f636aa76858f7f11b2a460f1dca9271
User2 <notescape262@gmail.com>
2023-10-20 13:31:03 +0200

    Добавьте файлы проекта.

---
+++ lab3rpm/MainWindow.xaml.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace lab3rpm
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            double sideA, sideB;
+            if (double.TryParse(SideATextBox.Text, out sideA) &&
+                double.TryParse(SideBTextBox.Text, out sideB))
+            {
+                Triangle triangle = null;
+                if (sideA!=0&&sideB!=0)
+                {
+                    triangle = new RightTriangle(sideA, sideB);
+                }
+                if (triangle != null)
+                {
+                    double area = triangle.CalculateArea();
+                    double perimeter = triangle.CalculatePerimeter();
+
+                    // Выводите результаты на экран, например, в Label или TextBox
+                    AreaResultLabel.Content = "Площадь: " + Math.Round(area, 4);
+                    PerimeterResultLabel.Content = "Периметр: " + Math.Round(perimeter, 4);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Пожалуйста, введите корректные числовые значения.");
+            }
+        }
+        private void Button_Click2(object sender, RoutedEventArgs e)
+        {
+            double sideA, angleAB;
+            if (double.TryParse(SideATextBox.Text, out sideA) &&
+                double.TryParse(AngleTextBox.Text, out angleAB))
+            {
+                Triangle triangle = null;
+                if (sideA != 0 && angleAB != 0)
+                {
+                    triangle = new IsoscelesTriangle(sideA, angleAB);
+                }
+                if (triangle != null)
+                {
+                    double area = triangle.CalculateArea();
+                    double perimeter = triangle.CalculatePerimeter();
+
+                    // Выводите результаты на экран, например, в Label или TextBox
+                    AreaResultLabel.Content = "Площадь: " + Math.Round(area, 4);
+                    PerimeterResultLabel.Content = "Периметр: " + Math.Round(perimeter, 4);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Пожалуйста, введите корректные числовые значения.");
+            }
+        }
+        private void Button_Click3(object sender, RoutedEventArgs e)
+        {
+            double sideA, angleAB;
+            if (double.TryParse(SideATextBox.Text, out sideA))
+            {
+                Triangle triangle = null;
+                if (sideA != 0)
+                {
+                    triangle = new EquilateralTriangle(sideA);
+                }
+                if (triangle != null)
+                {
+                    double area = triangle.CalculateArea();
+                    double perimeter = triangle.CalculatePerimeter();
+
+                    // Выводите результаты на экран, например, в Label или TextBox
+                    AreaResultLabel.Content = "Площадь: " + Math.Round(area, 4);
+                    PerimeterResultLabel.Content = "Периметр: " + Math.Round(perimeter, 4);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Пожалуйста, введите корректные числовые значения.");
+            }
+        }
+    }
+}
---
+++ lab3rpm/Triangle.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab3rpm
+{
+    abstract class Triangle
+    {
+        protected double SideA;
+        protected double SideB;
+        protected double AngleAB;
+
+        public Triangle(double sideA, double sideB, double angleAB)
+        {
+            SideA = sideA;
+            SideB = sideB;
+            AngleAB = angleAB;
+        }
+
+        public abstract double CalculateArea();
+        public abstract double CalculatePerimeter();
+    }
+    class RightTriangle : Triangle
+    {
+        public RightTriangle(double sideA, double sideB) : base(sideA, sideB, 90) { }
+
+        public override double CalculateArea()
+        {
+            return 0.5 * SideA * SideB;
+        }
+
+        public override double CalculatePerimeter()
+        {
+            return SideA + SideB + Math.Sqrt(SideA * SideA + SideB * SideB);
+        }
+    }
+
+    class IsoscelesTriangle : Triangle
+    {
+        public IsoscelesTriangle(double side, double baseAngle) : base(side, side, baseAngle) { }
+
+        public override double CalculateArea()
+        {
+            return 0.5 * SideA * SideA * Math.Sin(AngleAB);
+        }
+
+        public override double CalculatePerimeter()
+        {
+            return 2 * SideA + SideA * Math.Sin(AngleAB);
+        }
+    }
+
+    class EquilateralTriangle : IsoscelesTriangle
+    {
+        public EquilateralTriangle(double side) : base(side, 60) { }
+
+        public override double CalculateArea()
+        {
+            double n = SideA*Math.Sqrt(3)/2;
+            return SideA * n / 2;
+        }
+
+        public override double CalculatePerimeter()
+        {
+            return SideA + SideA + SideA;
+        }
+    }
+
+}
